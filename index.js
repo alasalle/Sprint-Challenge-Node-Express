817fef70c50f966e01b375b8fdd92ed411a91ff1
alasalle <angela.lasalle91@gmail.com>
2018-12-08 12:53:19 -0500

    added Routers completed custom middleware

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const server = express();
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const projectsRouter = require("./routers/projectsRouter");
+const actionsRouter = require("./routers/actionsRouter");
+
+const PORT = 4000;
+
+server.use(express.json());
+server.use(helmet());
+server.use(morgan("dev"));
+
+server.use("/api/posts", projectsRouter);
+server.use("/api/users", actionsRouter);
+
+
+server.listen(PORT, err => {
+  console.log(`server is now running on port ${PORT}`);
+});
---
+++ routers/projectsRouter.js
@@ -0,0 +1,5 @@
+const projectsModel = require("../data/helpers/projectModel");
+const customMW = require("../customMiddleware");
+
+const express = require('express');
+const router = express.Router();
